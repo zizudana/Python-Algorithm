d5b516df61e6dfa968e6e6728492c24f1dc65f38
zizudana <zizudana@naver.com>
2023-04-27 19:46:44 +0900

    Success: 문자열뒤집기,문자열숫자변환,소수판별

---
+++ Section2.구현/뒤집은_소수.py
@@ -0,0 +1,21 @@
+# n개의 자연수를 뒤집은 후 뒤집은 수가 소수이면 그 수를 출력
+def reverse(x): # 뒤집는 함수
+    return int(x[::-1])
+
+def isPrime(x): # 소수판별함수
+    flag = 1
+    for i in range(2, x//2+1):
+        if x % i == 0:
+            flag = 0
+            break
+    if flag:
+        return True
+    else:
+        return False
+
+n = int(input())
+nums = list(map(str, input().split()))
+for num in nums:
+    if isPrime(reverse(num)):
+        print(reverse(num), end=" ")
+    
