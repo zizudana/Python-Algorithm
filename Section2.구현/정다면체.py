e94be28aa1f17cc15168d7c691ff7e4ae066f0e0
zizudana <zizudana@naver.com>
2023-05-01 23:14:24 +0900

    Solution: Counter쓰지 않고 구하기

--- Section2.구현/정다면체.py
+++ Section2.구현/정다면체.py
@@ -16,3 +16,19 @@ for k, v in case_count:
     if v == max_count:
         print(k, end=" ")
 
+'''
+cnt = [0]*(n+m)
+for i in range(1, n+1):
+    for j in range(1, m+1):
+        cnt[i+j] += 1
+
+최댓값 찾기
+max = -2147000000
+for i in range(n+m+1):
+    if cnt[i] > max:
+        max = cnt[i]
+
+for i in range(n+m+1):
+    if cnt[i] == max:
+        print(i, ent = ' ')
+'''
