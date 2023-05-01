8c5631c0877ac373085cff34e2992af1bf82ade2
zizudana <zizudana@naver.com>
2023-05-01 23:13:13 +0900

    Solution: 에라토스테네스 체로 약수 개수 구하기

--- Section2.구현/소수_에라토스테네스_체.py
+++ Section2.구현/소수_에라토스테네스_체.py
@@ -10,3 +10,15 @@ for num in range(2, n):
     if flag:
         cnt += 1
 print(cnt)
+
+'''
+에라토스테네스 체
+ch=[0]*(n+1)
+cnt=0
+for i in range(2, n+1):
+    if ch[i]==0:
+        cnt+=1
+        for j in range(i, n+1, i):
+            ch[j]=1
+print(cnt)
+'''
