3c2596d13f38c2847187c3a2641c57a01b7eeed1
zizudana <zizudana@naver.com>
2023-05-01 23:20:17 +0900

    Success: reversed()로 뒤집은 결과는 리스트가 아니었다..

---
+++ Section3. 탐색&시뮬레이션/카드_역배치.py
@@ -0,0 +1,14 @@
+# 1부터 20까지 숫자가 쓰인 카드 오름차순으로 놓는다
+# 구간 [a, b] : 위치 a부터 b까지 카드를 현재의 역순으로 놓는다
+# 10개의 구간이 주어지면 순서대로 작업하고 마지막 배치를 구한다
+import sys
+sys.stdin=open("input.txt", "rt")
+
+card = [i for i in range(1, 21)]
+
+for _ in range(10):
+    a, b = map(int, input().split())
+    new_card = card[a-1:b]
+    card = card[:a-1] + new_card[::-1] + card[b:]
+
+print(card)
