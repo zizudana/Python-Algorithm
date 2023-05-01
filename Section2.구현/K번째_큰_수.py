ee2abe9a999a89360d64886f75fd9c51b96f5f3c
zizudana <zizudana@naver.com>
2023-05-01 23:10:34 +0900

    Solution: set 사용, set.add()

--- Section2.구현/K번째_큰_수.py
+++ Section2.구현/K번째_큰_수.py
@@ -7,3 +7,13 @@ card = list(map(int, input().split()))
 card.sort(reverse=True)
 card3 = list(combinations(card, 3))
 print(sum(card3[k-1]))
+
+'''
+중복 제거하며 3개 뽑고 더하기
+res = set()
+for i in range(n):
+    for j in range(i+1, n):
+        for m in range(j+1, n):
+            res.add(a[i]+a[j]+a[m])
+res = list(res)
+'''
