38781fa1fd5d31848ca56983f730150e010fa36e
zizudana <zizudana@naver.com>
2023-05-02 22:27:09 +0900

    Success: 판을 하나 더 만들어서 중복검사

---
+++ Section3. 탐색&시뮬레이션/스도쿠_검사.py
@@ -0,0 +1,22 @@
+# 완성된 9*9 크기의 수도쿠가 정확하게 풀렸는지 출력
+import sys
+sys.stdin=open("input.txt", "rt")
+
+board = list(list(map(int, input().split())) for _ in range(9))
+check = list([0 for _ in range(11)] for _ in range(9))
+
+flag = True
+for i in range(9):
+    for j in range(9):
+        idx = board[i][j]
+        if check[i][idx] == 0:
+            check[i][idx] = 1
+        else:
+            flag = False
+            break
+    if not flag:
+        break
+if(flag):
+    print("YES")
+else:
+    print("NO")
