7017aa83ca48e68ab69cb6d600cf54067fe0d08f
zizudana <zizudana@naver.com>
2023-05-05 21:18:52 +0900

    Solution: 정렬하여 첫번째값을 답에 추가하고 tmp.clear()

--- Section4.이분탐색/증가수열_만들기.py
+++ Section4.이분탐색/증가수열_만들기.py
@@ -23,3 +23,21 @@ while len(arrs) > 0:
             before = arrs.pop(0)
 print(len(answer))
 print(answer)
+'''
+while lt <= rt:
+    if a[lt] > last:
+        tmp.append((a[lt], 'L'))
+    if a[rt] > last:
+        tmp.append((a[rt], 'R'))
+    tmp.sort()
+    if len(tmp) == 0:
+        break
+    else:
+        res += tmp[0][1]
+        last = tmp[0][0]
+        if tmp[0][1] == 'L':
+            lt += 1
+        else:
+            rt -= 1
+    tmp.clear()
+'''
