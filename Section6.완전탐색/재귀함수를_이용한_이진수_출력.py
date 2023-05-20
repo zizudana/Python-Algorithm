1b32918a5dce70693287bce3208ccaa2e4119a09
zizudana <zizudana@naver.com>
2023-05-20 14:33:38 +0900

    Solution: 출력을 호출 다음에 하면 거꾸로 출력할수있다

--- Section6.완전탐색/재귀함수를_이용한_이진수_출력.py
+++ Section6.완전탐색/재귀함수를_이용한_이진수_출력.py
@@ -1,13 +1,11 @@
 # 10진수 n을 2진수로 변환하여 출력하는 프로그램
 
 n = int(input())
-ans = []
+
 def recur(num):
     if num==0:
         return
-    ans.append(num%2)
     recur(num//2)
+    print(num%2, end="")
 
 recur(n)
-for i in reversed(ans):
-    print(i, end="")
