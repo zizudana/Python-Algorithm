c0f4da5c2cdbabe845ce6e1036858d8a40413ab9
zizudana <zizudana@naver.com>
2023-05-20 14:55:32 +0900

    Solution: DFS풀이

--- Section6.완전탐색/부분집합_구하기.py
+++ Section6.완전탐색/부분집합_구하기.py
@@ -8,3 +8,25 @@ for i in range(1, n+1):
     ans += map(list,combinations(nums, i)) 
 
 print(ans)
+
+def DFS(v):
+    if v==n+1:
+        for i in range(1, n+1):
+            if ch[i]==1:
+                print(i, end=' ')
+        print()
+    else:
+        ch[v]=1
+        DFS(v+1)
+        ch[v]=0
+        DFS(v+1)
+
+ch=[0]*(n+1)
+DFS(1)
+'''
+한 원소가 포함된다/안된다 2가지 경우(상태트리)
+총 경우의수=2^n -1
+'''
+
+    
+    
