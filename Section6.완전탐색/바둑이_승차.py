7aac83dac2f22718d60f3a6ab54b65d1bd2f3f6c
zizudana <zizudana@naver.com>
2023-05-20 15:53:42 +0900

    Solution: 최적화

--- Section6.완전탐색/바둑이_승차.py
+++ Section6.완전탐색/바둑이_승차.py
@@ -5,20 +5,27 @@ import sys
 sys.stdin=open("input.txt", "rt")
 
 
-def DFS(v, s):
+def DFS(v, s, tsum):
     global max_s
+    if s+(total-tsum) < max_s:
+        return
     if s > c:
         return
     if v==n:
         max_s = max(max_s, s)
     else:
-        DFS(v+1, s+w[v])
-        DFS(v+1, s)
+        DFS(v+1, s+w[v], tsum+w[v])
+        DFS(v+1, s, tsum+w[v])
 
 c, n = map(int, input().split())
 w = []
 for _ in range(n):
     w.append(int(input()))
 max_s = 0
-DFS(0, 0)
+total = sum(w)
+DFS(0, 0, 0)
 print(max_s)
+
+'''
+최적화 - 남은 것들을 다 더해도 현재 max값보다 작으면 더 해볼 필요가 없음
+'''
