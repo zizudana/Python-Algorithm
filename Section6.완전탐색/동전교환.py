835514f4773cf6cabe2bd73f37b99d4eb898cbd0
zizudana <zizudana@naver.com>
2023-05-25 22:09:19 +0900

    Solution: dfs가 아닌 그리드로 푼거같다

---
+++ Section6.완전탐색/동전교환.py
@@ -0,0 +1,17 @@
+# 여러 단위의 동전들이 있을 때 거스름돈을 가장 적은 수의 동전으로 주려면 어떻게 줘야 하는가
+import sys
+sys.stdin=open("input.txt", "rt")
+
+n = int(input())
+coins= list(map(int, input().split()))
+m = int(input())
+cnt= 0
+coins.sort(reverse=True)
+for coin in coins:
+    if coin <= m and m > 0:
+        cnt += m//coin
+        m %= coin
+    else:
+        break
+print(cnt)
+
